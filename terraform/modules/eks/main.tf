9ff6b05fb54885a822256b318d0121d318a8faa0
prabalpratap191 <prabalpratap191@gmail.com>
2026-08-20 11:35:11 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,1 +1,21 @@
+module "eks" {
 
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.33"
+
+  eks_managed_node_groups = {
+
+    default = {
+
+      desired_size = var.node_desired
+      min_size     = var.node_min
+      max_size     = var.node_max
+
+      instance_types = [
+        var.node_instance_type
+      ]
+    }
+  }
+}
