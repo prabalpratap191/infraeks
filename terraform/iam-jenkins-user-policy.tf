d3115d79632d722c4f23e38c178d4580f459152e
prabalpratap191 <prabalpratap191@gmail.com>
2026-08-20 16:37:44 +0530

    Update iam-jenkins-user-policy.tf

--- terraform/iam-jenkins-user-policy.tf
+++ terraform/iam-jenkins-user-policy.tf
@@ -1,70 +1,70 @@
 # IAM Policy for Jenkins User - KMS and CloudWatch Logs Permissions
 # This policy grants the jenkins-user the necessary permissions to create
 # and manage KMS keys and CloudWatch Log Groups for EKS cluster
-
-resource "aws_iam_policy" "jenkins_eks_permissions" {
-  name        = "JenkinsEKSAdditionalPermissionsUser"
-  description = "Additional permissions for Jenkins user to create EKS cluster with KMS and CloudWatch Logs"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "KMSPermissions"
-        Effect = "Allow"
-        Action = [
-          "kms:CreateKey",
-          "kms:DescribeKey",
-          "kms:GetKeyPolicy",
-          "kms:PutKeyPolicy",
-          "kms:TagResource",
-          "kms:UntagResource",
-          "kms:ScheduleKeyDeletion",
-          "kms:CancelKeyDeletion",
-          "kms:EnableKeyRotation",
-          "kms:DisableKeyRotation",
-          "kms:CreateAlias",
-          "kms:DeleteAlias",
-          "kms:UpdateAlias",
-          "kms:ListAliases",
-          "kms:ListResourceTags"
-        ]
-        Resource = "*"
-      },
-      {
-        Sid    = "CloudWatchLogsPermissions"
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:DescribeLogGroups",
-          "logs:PutRetentionPolicy",
-          "logs:DeleteLogGroup",
-          "logs:ListTagsLogGroup",
-          "logs:TagLogGroup",
-          "logs:UntagLogGroup",
-          "logs:TagResource",
-          "logs:UntagResource"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-
-  tags = {
-    Purpose   = "EKS Cluster Management"
-    ManagedBy = "Terraform"
-  }
-}
+#
+#resource "aws_iam_policy" "jenkins_eks_permissions" {
+#  name        = "JenkinsEKSAdditionalPermissions"
+#  description = "Additional permissions for Jenkins user to create EKS cluster with KMS and CloudWatch Logs"
+#
+#  policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [
+#      {
+#        Sid    = "KMSPermissions"
+#        Effect = "Allow"
+#        Action = [
+#          "kms:CreateKey",
+#          "kms:DescribeKey",
+#          "kms:GetKeyPolicy",
+#          "kms:PutKeyPolicy",
+#          "kms:TagResource",
+#          "kms:UntagResource",
+#          "kms:ScheduleKeyDeletion",
+#          "kms:CancelKeyDeletion",
+#          "kms:EnableKeyRotation",
+#          "kms:DisableKeyRotation",
+#          "kms:CreateAlias",
+#          "kms:DeleteAlias",
+#          "kms:UpdateAlias",
+#          "kms:ListAliases",
+#          "kms:ListResourceTags"
+#        ]
+#        Resource = "*"
+#      },
+#      {
+#        Sid    = "CloudWatchLogsPermissions"
+#        Effect = "Allow"
+#        Action = [
+#          "logs:CreateLogGroup",
+#          "logs:DescribeLogGroups",
+#          "logs:PutRetentionPolicy",
+#          "logs:DeleteLogGroup",
+#          "logs:ListTagsLogGroup",
+#          "logs:TagLogGroup",
+#          "logs:UntagLogGroup",
+#          "logs:TagResource",
+#          "logs:UntagResource"
+#        ]
+#        Resource = "*"
+#      }
+#    ]
+#  })
+#
+#  tags = {
+#    Purpose   = "EKS Cluster Management"
+#    ManagedBy = "Terraform"
+#  }
+#}
 
 # Attach the policy to jenkins-user
 # Note: You need to import the existing jenkins-user or manage it separately
 # Uncomment and configure the following if you want to manage the attachment:
 
- data "aws_iam_user" "jenkins" {
-   user_name = "jenkins-user"
- }
+# data "aws_iam_user" "jenkins" {
+#   user_name = "jenkins-user"
+# }
 
- resource "aws_iam_user_policy_attachment" "jenkins_eks_permissions" {
-   user       = data.aws_iam_user.jenkins.user_name
-   policy_arn = aws_iam_policy.jenkins_eks_permissions.arn
- }
+# resource "aws_iam_user_policy_attachment" "jenkins_eks_permissions" {
+#   user       = data.aws_iam_user.jenkins.user_name
+#   policy_arn = aws_iam_policy.jenkins_eks_permissions.arn
+# }
