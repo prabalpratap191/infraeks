a912f0a6cb850f6102e7b2c0225eda8ab8be784e
prabalpratap191 <prabalpratap191@gmail.com>
2026-08-20 11:34:31 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,9 @@
+provider "aws" {
+  region = var.region
+}
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
