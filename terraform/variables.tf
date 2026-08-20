9fe2588acdcf2748d2f82dcd2c3709916d3f6892
prabalpratap191 <prabalpratap191@gmail.com>
2026-08-20 11:34:51 +0530

    Add multiple variables to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,8 @@
-
+variable "cluster_name" {}
+variable "region" {}
+variable "namespace" {}
+variable "service_account" {}
+variable "node_instance_type" {}
+variable "node_desired" {}
+variable "node_min" {}
+variable "node_max" {}
