9aa040eeb93ad05776f6136d042a4dbd73bd8ac7
prabalpratap191 <prabalpratap191@gmail.com>
2026-08-20 12:39:28 +0530

    Refactor EKS module to use default VPC and subnets
    Updated EKS module configuration to use default VPC and subnets, and specified cluster version.

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,23 +1,45 @@
-terraform {
-  required_version = ">= 1.0.0"
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
 }
 
-provider "aws" {
-  region = var.region
+# Get default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
-# EKS Cluster Module
 module "eks" {
-  source = "./modules/eks"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
 
-  cluster_name        = var.cluster_name
-  node_instance_type  = var.node_instance_type
-  node_desired        = var.node_desired
-  node_min            = var.node_min
-  node_max            = var.node_max
-}
+  cluster_name    = var.cluster_name
+  cluster_version = "1.28"
 
-# Data source for EKS cluster authentication
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_name
+  cluster_endpoint_public_access = true
+
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.default.ids
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = var.node_desired
+      min_size     = var.node_min
+      max_size     = var.node_max
+
+      instance_types = [var.node_instance_type]
+
+      tags = {
+        Environment = "dev"
+        Terraform   = "true"
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
