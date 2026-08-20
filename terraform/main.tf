38350bd86fd6d0f709d9d3b8e0f87da285cff423
prabalpratap191 <prabalpratap191@gmail.com>
2026-08-20 12:40:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,5 +3,21 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
+}
+
+# EKS Cluster Module
+module "eks" {
+  source = "./modules/eks"
+
+  cluster_name        = var.cluster_name
+  node_instance_type  = var.node_instance_type
+  node_desired        = var.node_desired
+  node_min            = var.node_min
+  node_max            = var.node_max
+}
+
+# Data source for EKS cluster authentication
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_name
 }
