bedd1cec57c25cd69654ff386c7916c350611779
prabalpratap191 <prabalpratap191@gmail.com>
2026-08-20 12:54:06 +0530

    Update main.tf

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -13,7 +13,7 @@ data "aws_subnets" "default" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+  version = "~> 20.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
